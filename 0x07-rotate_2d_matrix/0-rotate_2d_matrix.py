f45b14d46d79e723bee8a6bc9b8c2477b500ea5f
Rophpad <rophenp@gmail.com>
2024-04-22 12:56:55 +0100

    [+] add rotate_2d_matrix()

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Rotation of 2D Matrix in Python
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Rotates an n x n 2D matrix
+        90 degree clockwise
+    """
+    for x, y in enumerate(zip(*reversed(matrix))):
+        matrix[x] = list(y)
