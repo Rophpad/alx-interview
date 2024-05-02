eb895ba591c441b8013aa1f318c991fff6cbe6a2
Rophpad <rophenp@gmail.com>
2024-05-02 14:52:45 +0100

    [+] makeChange() implemented

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Dynamic programming and greedy algorithms:
+    the coin change problem
+"""
+
+
+def makeChange(coins, total):
+    """ Determine the fewest number of coins needed
+        to meet total
+    """
+    if total <= 0:
+        return 0
+
+    num_of_coins = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = total // coin
+        num_of_coins += r
+        total -= r * coin
+        if total == 0:
+            return num_of_coins
+    return -1
